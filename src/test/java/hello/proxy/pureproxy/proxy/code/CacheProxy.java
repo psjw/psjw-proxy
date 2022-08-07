bd6923542ccd22589c3656c09c771cce6eefff61
psjw <dlrudtn108@gmail.com>
2022-08-07 15:41:25 +0900

    Step01_ProxyPatternSample

---
+++ src/test/java/hello/proxy/pureproxy/proxy/code/CacheProxy.java
@@ -0,0 +1,12 @@
+package hello.proxy.pureproxy.proxy.code;/**
+ * packageName : hello.proxy.pureproxy.proxy.code
+ * fileName : CacheProxy
+ * author : psjw
+ * date : 2022-08-07
+ * description :
+ * ===========================================================
+ * DATE              AUTHOR          NOTE
+ * -----------------------------------------------------------
+ * 2022-08-07        psjw         최초 생성
+ */public class CacheProxy {
+}
---
+++ src/test/java/hello/proxy/pureproxy/proxy/code/Subject.java
@@ -0,0 +1,12 @@
+package hello.proxy.pureproxy.proxy.code;/**
+ * packageName : hello.proxy.pureproxy.proxy.code
+ * fileName : Subject
+ * author : psjw
+ * date : 2022-08-07
+ * description :
+ * ===========================================================
+ * DATE              AUTHOR          NOTE
+ * -----------------------------------------------------------
+ * 2022-08-07        psjw         최초 생성
+ */public interface Subject {
+}
